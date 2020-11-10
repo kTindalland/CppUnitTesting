8dcb556c26721f3c2879d9cc15969b41432afbf5
Kai Tindall <kaitindall2000@gmail.com>
2020-11-10 15:26:32 +0000

    removed out

--- out/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kai/googletesting
-# Build directory: /home/kai/googletesting/out/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
-subdirs("test")
--- out/build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kai/googletesting/googletest
-# Build directory: /home/kai/googletesting/out/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
