4482deb36e5c4b09bcdc341a220c32f11145f7dc
Kai Tindall <kaitindall2000@gmail.com>
2020-11-10 15:21:28 +0000

    Init

---
+++ out/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/kai/googletesting
+# Build directory: /home/kai/googletesting/out/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googletest")
+subdirs("test")
---
+++ runTests.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+#make -C out/build test
+cd out/build/test; ./ExampleTests
